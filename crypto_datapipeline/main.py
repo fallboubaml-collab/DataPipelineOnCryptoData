7d657ab91cfe86825b96b4509c567e69d7bd0a83
FallBFAll <6193094@cegepmontpetit.ca>
2026-02-27 03:30:39 -0500

    FCT:Added the logic for the data transformation and a bit of feature engineering

--- crypto_datapipeline/main.py
+++ crypto_datapipeline/main.py
@@ -1,14 +1,16 @@
 from src.ingestion import ingest_5s
 from apscheduler.schedulers.blocking import BlockingScheduler
+from src.transform import preprocessing_5s_dataframe
 
 def main():
 #Step 1:Ingestion of Raw Data
 #using this scheduler to call function every 5 secs
-    scheduler = BlockingScheduler()
+    #scheduler = BlockingScheduler()
     #function not call just reference
-    scheduler.add_job(ingest_5s,trigger='interval',seconds=5)
-    scheduler.start()
-       
+    #scheduler.add_job(ingest_5s,trigger='interval',seconds=5)
+    #scheduler.start()
+
+       preprocessing_5s_dataframe()
 
 if __name__ == "__main__":
     main()
--- crypto_datapipeline/src/transform.py
+++ crypto_datapipeline/src/transform.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import json
+import glob
+import os
+
 
 #Function to transform to dataframe
 def loading_in_pd(day):
@@ -31,23 +34,59 @@ def preprocessing_dataframe():
     df.to_csv("data/processed/{time_frame}", index=False)
 
 
-def loading_coins_pd(coins="bitcoin",lastdays=30):
 
-    #Load raw JSON
-    with open(f"data/raw/{coins}last{lastdays}days.json") as f:
-        data = json.load(f)
-    #transform to Dataframe
-    df = pd.DataFrame(data)
+def loading_5sec_pd():
+    folder_path = "../data/raw/MFC"
 
-    # Keep relevant columns
-    df = df[['id','symbol','name','current_price','market_cap','total_volume','price_change_percentage_24h']]
+    #hold json file in folder
+    json_files = glob.glob(os.path.join(folder_path, "*.json"))
+    print("Files found:", len(json_files))
 
-    return {
-        "data": df,
-        "coin": coins,
-        "days": lastdays
-    }
+    list_of_dfs = []
+
+        #iterate in all file to convert as a df in list_of_df
+    for file_path in json_files:
+        try:
+            df = pd.read_json(file_path, lines=True)
+            list_of_dfs.append(df)
+        except Exception as e:
+             print(f"Failed on {file_path}: {e}")
+
+    #list of df concatonate df to make a full on dataframe of all df
+    if list_of_dfs:
+        master_df = pd.concat(list_of_dfs, ignore_index=True)
+        print("Final shape:", master_df.shape)
+        display(master_df.head())
+    else:
+     print("No valid JSON data loaded.")
+     return master_df
+    
 
 
+def preprocessing_5s_dataframe():
+
+    #load df
+    df = loading_5sec_pd()
+    #drop NAN value
+    df.dropna()
+
+    # Folder to save processed DataFrame
+    output_folder = "../data/processed"
+    os.makedirs(output_folder, exist_ok=True)
+
+    # File path
+    output_file = os.path.join(output_folder, "mdf_data.csv")
+
+    # Save DataFrame
+    df.to_csv(output_file, index=False)
+    print(f"Data saved to {output_file}")
+
+
+
+
+
+
+ 
+    
 
 
